a5a62cc14b09941376ec7d841e3867f8e3a7a161
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-07-27 11:04:11 -0700

    Edited pH Analyzer

--- ph-analyzer/main.js
+++ ph-analyzer/main.js
@@ -3,7 +3,7 @@ function phAnalyzer(ph) {
         return 'neutral';
     } else if (ph >= 0 && ph <=6) {
         return 'acidic';
-    } else if (ph >= 7 && ph <= 14) {
+    } else if (ph > 7 && ph <= 14) {
         return 'alkaline';
     } else {
         return 'invalid pH value';
